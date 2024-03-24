3a9d7efa254aadee636d9d5216d4d926bff41b07
Lucas Barros <lcsavb@gmail.com>
2024-03-24 07:27:45 +0100

    improving

--- src/lib.rs
+++ src/lib.rs
@@ -4,48 +4,29 @@ use num::integer::gcd;
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut triplets = HashSet::new();
     // Euclid's formula
-    // a = m^2 - n^2, b = 2mn, sum = m^2 + n^2
-    // m and n must have different parity
-    // the greatest common divisor of m and n must be 1
+    // a = m^2 - n^2, b = 2mn, c = m^2 + n^2
+    
 
-
-
-    find_squares(sum as u32)
-        .iter()
-        .for_each(|&(m, n)| {
-            if have_different_parity(m, n) && are_coprime(m, n) {
-                let a = m.pow(2) - n.pow(2);
+    // O(n²) i will try to optimize it
+    for m in 2..sum {
+        for n in 1..m {
+            
+                let a = m * m - n * n;
                 let b = 2 * m * n;
-                triplets.insert([a, b, sum]);
-            }
-
-        });
-
-    triplets
-
-
-}
-
-
-
-fn are_coprime(m: u32, n: u32) -> bool {
-    gcd(m, n) == 1
-}
-
-fn have_different_parity(m: u32, n: u32) -> bool {
-    m % 2 != n % 2
-}
-
-
-pub fn find_squares(sum: u32) -> Vec<(u32, u32)> {
-    let mut squares = Vec::new();
-
-    for m in 1..sum {
-        for n in 1..sum {
-            if m.pow(2) + n.pow(2) == sum {
-                squares.push((m, n));
-            }
+                let c = m * m + n * n;
+                if a + b + c == sum {
+                    let mut triplet = [a, b, c];
+                    triplet.sort();
+                    triplets.insert(triplet);
+                }
+            
         }
     }
-    squares
-}
+    triplets
+} 
+
+
+// the previous error that only PRIMITIVE TRIPLETS have to
+// respect the following conditions:
+// m and n must have different parity
+// the greatest common divisor of m and n must be 1
