365a56ad460d904ac359c5fc1f5c186996cb9b1a
Lucas Barros <lcsavb@gmail.com>
2024-03-24 07:36:09 +0100

    tests passing

--- src/lib.rs
+++ src/lib.rs
@@ -3,30 +3,35 @@ use num::integer::gcd;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut triplets = HashSet::new();
-    // Euclid's formula
-    // a = m^2 - n^2, b = 2mn, c = m^2 + n^2
-    
 
-    // O(n²) i will try to optimize it
+    // Precompute squares
+    let squares: Vec<u32> = (0..=sum).map(|x| x * x).collect();
+
+
     for m in 2..sum {
         for n in 1..m {
-            
-                let a = m * m - n * n;
-                let b = 2 * m * n;
-                let c = m * m + n * n;
-                if a + b + c == sum {
-                    let mut triplet = [a, b, c];
-                    triplet.sort();
-                    triplets.insert(triplet);
+            if (m - n) % 2 == 1 && gcd(m, n) == 1 {
+                for k in 1..=sum / (2 * m * m) {
+                    let a = k * (squares[m as usize] - squares[n as usize]);
+                    let b = k * 2 * m * n;
+                    let c = k * (squares[m as usize] + squares[n as usize]);
+                    if a + b + c == sum {
+                        let mut triplet = [a, b, c];
+                        triplet.sort();
+                        triplets.insert(triplet);
+                    }
                 }
-            
+            }
         }
     }
+
     triplets
-} 
+}
+
 
 
 // the previous error that only PRIMITIVE TRIPLETS have to
 // respect the following conditions:
 // m and n must have different parity
 // the greatest common divisor of m and n must be 1
+// moreover I forgot to add the multiplication factor k
